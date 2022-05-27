19a37338642f1af1d56245a2d32f74b9ab22c2ac
Pierre <88279582+yvpierre@users.noreply.github.com>
2022-05-27 22:14:53 +0200

    Movie card firsts elems

--- src/App.vue
+++ src/App.vue
@@ -1,119 +1,55 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <body>
+  <div class="card">
+    <div class="desc--short">
+      <div class="desc--short--1">
+        <div class="bloc-titre">
+          <div class="titre">Goodfellas</div>
+          <div class="annee">(1990)</div>
+        </div>
+        <div class="note">9.9/10</div>
+      </div>
+      <div class="desc--short--2">
+        <div class="real">by <u>Martin Scorsese</u></div>
+        <div class="btn-add btn--pri">Add</div>
+      </div>
     </div>
-  </header>
+    <div class="desc--long">
+      <div class="desc--long--1">
+        <div class="bloc-titre">
+          <div class="titre">Goodfellas</div>
+          <div class="annee">(1990)</div>
+        </div>
+        <div class="note">9.9/10</div>
+      </div>
+      <div class="desc--long--2">
+        <div class="desc--note">Your note (241)</div>
+        <div class="note--info">?</div>
+        <div class="note--user">9&nbsp;/&nbsp;10</div>
+      </div>
+      <div class="long--director">Director&nbsp;:&nbsp;Martin Scorsese</div>
+      <div class="long--cast">
+        Cast&nbsp;:&nbsp;Robert de Niro, Ray Liotta, Joe Pesci, more..
+      </div>
+      <div class="long--plot">
+        Synopsis&nbsp;:&nbsp;Young Henry Hill, with his friends Jimmy and
+        Tommy, begins the climb from being a petty criminal to a gangster on
+        the mean streets of New York.
+      </div>
+      <div class="long--btns">
+        <div class="long--btnInfo">+&nbsp;Infos</div>
+        <div class="long--btnAdd">Add</div>
+      </div>
+    </div>
+  </div>
+  </body>
 
-  <RouterView />
+
 </template>
 
 <style>
 @import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
+
+// @ts-ignore
+const card:HTMLElement = document.querySelector(".card");
+// @ts-ignore
+const shortDesc:HTMLElement = document.querySelector(".desc--short");
+// @ts-ignore
+const longDesc:HTMLElement = document.querySelector(".desc--long");
+
+let cardState = false;
+
+console.log(getComputedStyle(card).display);
+
+card.addEventListener("click", function () {
+  cardState ? (cardState = false) : (cardState = true);
+  toggleDisplay(cardState);
+});
+
+function toggleDisplay(state) {
+  if (state) {
+    shortDesc.style.setProperty("display", "none");
+    longDesc.style.setProperty("display", "block");
+    card.style.setProperty("justify-content", "flex-start");
+  } else {
+    shortDesc.style.setProperty("display", "flex");
+    longDesc.style.setProperty("display", "none");
+    card.style.setProperty("justify-content", "flex-end");
+  }
+}
