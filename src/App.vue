e0ef7ee293232ac3b656b8c96e11a3b6cc3d0fe9
Pierre <88279582+yvpierre@users.noreply.github.com>
2022-06-24 11:55:43 +0200

    adding hero banner

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import HelloWorld from "@/components/HelloWorld.vue";
 import WelcomeItem from "@/components/WelcomeItem.vue";
 import Card from "@/components/MovieCard.vue";
 import NavbarComponent from "@/components/NavbarComponent.vue";
+import HeroComponent from "@/components/HeroComponent.vue";
 
 </script>
 
 <template>
   <body class="body">
     <NavbarComponent />
+    <HeroComponent />
     <div class="main--cards">
       <Card v-for="n in 10" :key="n"/>
     </div>
